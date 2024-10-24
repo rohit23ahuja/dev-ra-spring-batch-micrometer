212e55b715b1c4b2cade52d75f9fa8c2f95ddf50
rohit23ahuja <rohit23ahuja@gmail.com>
2024-10-24 12:05:27 +0530

    initial commit

---
+++ src/main/java/dev/ra/springbatch/micrometer/JobConfiguration.java
@@ -0,0 +1,13 @@
+package dev.ra.springbatch.micrometer;
+
+import org.springframework.batch.core.Job;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobConfiguration {
+	
+	public Job configureJob() {
+		return null;
+	}
+
+}
--- src/main/java/dev/ra/springbatch/micrometer/SpringBatchMicrometer.java
+++ src/main/java/dev/ra/springbatch/micrometer/SpringBatchMicrometer.java
@@ -5,28 +5,31 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringBatchMicrometer {
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("job/footballJob.xml");
+	public static void main(String[] args) {
+		String jobName = args[0];
+		System.out.println("Job name: " + jobName);
+		if (jobName.equals("footballJobXml")) {
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("job/footballJob.xml");
 
-        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-        Job job = (Job) context.getBean("footballJob");
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters();
-        try {
-            JobExecution execution = jobLauncher.run(job, jobParameters);
-            System.out.println("Exit Status : " + execution.getStatus());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Done");
-        context.close();
+			JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+			Job job = (Job) context.getBean("footballJob");
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+					.toJobParameters();
+			try {
+				JobExecution execution = jobLauncher.run(job, jobParameters);
+				System.out.println("Exit Status : " + execution.getStatus());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("Done");
+			context.close();
+		} else {
+			
+		}
 
-
-    }
+	}
 }
