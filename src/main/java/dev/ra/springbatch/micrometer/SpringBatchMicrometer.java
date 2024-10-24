5e36dbb408ad1143f1753a67c92cb3cca3a8b46d
rohit23ahuja <rohit23ahuja@gmail.com>
2024-10-24 12:41:06 +0530

    late binding initial commit

--- src/main/java/dev/ra/springbatch/micrometer/SpringBatchMicrometer.java
+++ src/main/java/dev/ra/springbatch/micrometer/SpringBatchMicrometer.java
@@ -5,18 +5,20 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringBatchMicrometer {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("job/footballJob.xml");
-
+        String jobName = args[0];
+        
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
         Job job = (Job) context.getBean("footballJob");
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
+                .addString("jobName", jobName)
+                .addString("names", "ID,lastName,firstName,position,birthYear,debutYear")
                 .toJobParameters();
         try {
             JobExecution execution = jobLauncher.run(job, jobParameters);
